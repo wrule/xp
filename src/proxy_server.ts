482b9fc708539a46d7edf9b65275ffaf111efbe3
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-06 17:25:40 +0800

    提交代码

--- src/proxy_server.ts
+++ src/proxy_server.ts
@@ -1,7 +1,8 @@
 import * as mockttp from 'mockttp';
 import { ProxyPort } from './global_proxy';
+import ca from './ca';
 
 function Start() {
-  const proxy_server = mockttp.getLocal();
+  const proxy_server = mockttp.getLocal({ https: ca });
   proxy_server.start(ProxyPort);
 }
