6dcd0bc99183e69ee15a10802dd693963e955671
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-06 18:24:47 +0800

    提交代码

--- src/api_server.ts
+++ src/api_server.ts
@@ -1,7 +1,16 @@
 import http from 'http';
 import express from 'express';
+import { CloseProxy, OpenProxy } from './global_proxy';
 
 const app = express();
+const router = express.Router();
+router.post('/proxy/open', async (req, res) => {
+  res.json(await OpenProxy());
+});
+router.post('/proxy/close', async (req, res) => {
+  res.json(await CloseProxy());
+});
+app.use('/api', router);
 const api_server = http.createServer(app);
 
 export const ApiPort = 38502;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Start, Stop } from './proxy_server';
+import * as ApiServer from './api_server';
 
 function main() {
+  ApiServer.Start();
   Start();
-  setTimeout(() => {
-    Stop();
-  }, 10000);
+  // setTimeout(() => {
+  //   Stop();
+  // }, 10000);
 }
 
 main();
