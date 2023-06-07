688a664b6122a505b2b229a496cd446608ea73d0
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-07 14:03:47 +0800

    提交代码

--- src/proxy_server.ts
+++ src/proxy_server.ts
@@ -5,21 +5,20 @@ import { IO } from './socket_server';
 import * as RequestStore from './request_store';
 
 const proxy_server = mockttp.getLocal({ https: ca });
-proxy_server.on('request', (request) => {
-  RequestStore.UpdateRequest(request);
-  IO.emit('request', { ...request, body: undefined });
-  console.log(request.id, request.url);
-});
-proxy_server.on('response', (response) => {
-  RequestStore.UpdateResponse(response);
-  IO.emit('response', { ...response, body: undefined });
-});
-
 let started = false;
 
 export
 async function Start() {
   if (started) return;
+  proxy_server.on('request', (request) => {
+    RequestStore.UpdateRequest(request);
+    IO.emit('request', { ...request, body: undefined });
+    console.log(request.id, request.url);
+  });
+  proxy_server.on('response', (response) => {
+    RequestStore.UpdateResponse(response);
+    IO.emit('response', { ...response, body: undefined });
+  });
   proxy_server.forAnyRequest().thenPassThrough();
   await proxy_server.start(ProxyPort);
   await OpenProxy();
