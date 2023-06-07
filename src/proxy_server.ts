27b4e190413e55e2deb9cfd33b56ece25c242ee2
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-07 13:56:42 +0800

    提交代码

--- src/proxy_server.ts
+++ src/proxy_server.ts
@@ -15,17 +15,23 @@ proxy_server.on('response', (response) => {
   IO.emit('response', { ...response, body: undefined });
 });
 
+let started = false;
+
 export
 async function Start() {
+  if (started) return;
   proxy_server.forAnyRequest().thenPassThrough();
   await proxy_server.start(ProxyPort);
   await OpenProxy();
   console.log('proxy_server started on port', ProxyPort, '...');
+  started = true;
 }
 
 export
 async function Stop() {
+  if (!started) return;
   await proxy_server.stop();
   await CloseProxy();
   console.log('proxy_server has stopped');
+  started = false;
 }
